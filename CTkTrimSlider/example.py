332571d098ce530ab54f0fa7abb5da32e7d065f6
gindd <gingerichdvd@gmail.com>
2026-04-07 20:26:54 +0900

    feat: Add args and functions to the example

--- CTkTrimSlider/example.py
+++ CTkTrimSlider/example.py
@@ -2,10 +2,18 @@ import customtkinter as ctk
 
 from ctk_trimslider import CTkTrimSlider
 
+def show_lvalue(value):
+    print(value)
+
+def show_rvalue(value):
+    print(value)
+
+def show_cvalue(value):
+    print(value)
+
 root = ctk.CTk()
 
-
-range_slider = CTkTrimSlider(root)
-range_slider.pack(padx=30, pady=30, fill="both")
+trim_slider = CTkTrimSlider(root, to=100, from_=0, number_of_steps=1000, lbutton_command=show_lvalue, rbutton_command=show_rvalue, cbutton_command=show_cvalue)
+trim_slider.pack(padx=30, pady=30, fill="both")
 
 root.mainloop()
