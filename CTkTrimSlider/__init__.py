6be57d1fe8bb30fe1eed69311b55db202c3079cb
gindvd <gingerichdvd@gmail.com>
2026-05-05 21:28:33 +0900

    fix: Update version number

--- CTkTrimSlider/__init__.py
+++ CTkTrimSlider/__init__.py
@@ -4,6 +4,6 @@ Specialized 3 button range slider for customtkinter
 Author: David Gingerich
 """
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from CTkTrimSlider.ctk_trimslider import CTkTrimSlider
